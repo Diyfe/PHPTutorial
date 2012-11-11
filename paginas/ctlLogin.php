a393e228775a0e05611226daded2eea38cf5e5c1
Wilman Jose Vega Castilla <wilmanvega@gmail.com>
2012-11-11 11:19:45 -0500

    - Se corrigieron unos errores en el archivo - Se agregaron los comentarios respectivos

--- paginas/ctlLogin.php
+++ paginas/ctlLogin.php
@@ -1,8 +1,12 @@
 <?php
+/* He modificado este archivo.
+ * Notese que el editor coloca unas marcas, que indican 
+ * que se esta realizando un cambio al archivo.
+ */
 include dirname(__FILE__) . '\..\modelo\Usuario.php';
 include dirname(__FILE__) . '\..\modelo\Mapeador.php';
 include dirname(__FILE__) . '\..\dao\UsuarioDAO.php';
-$mensaje = "";
+$mensaje = "  ";
 session_start();
 if (array_key_exists("usuario", $_POST)) {
     $usuarioP = $_POST['usuario'];
@@ -13,8 +17,6 @@ if (array_key_exists("usuario", $_POST)) {
     if($usuario == null){
         $mensaje="Usuario/Clave no encontrados";
     }else{
-        $_SESSION['usuario'] = $usuario->getUsuario();
-        $_SESSION['nombre'] = $usuario->getNombre();
         header('Location: home.php');
         die();
     }
